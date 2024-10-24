8b69c3885076cb1b6fc696c63f0b2446eb97fb4f
Aadi Umrani <aadi.p.um@gmail.com>
2024-10-24 11:55:23 -0400

    first commit

---
+++ src/main/java/com/iotdemo/lightswitch/LightswitchApplication.java
@@ -0,0 +1,13 @@
+package com.iotdemo.lightswitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightswitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightswitchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iotdemo/lightswitch/LightswitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iotdemo.lightswitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightswitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
